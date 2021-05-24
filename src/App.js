e645b5fdff640ef029fbf6df3577a56d5ef7e6d3
aman adinlid <95pinkpanda@gmail.com>
2021-05-24 17:06:42 +0200

    Edited it

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import Jokes from './components/Jokes';
 import './App.css';
 
 function App() {
-  <div className='App'>
-    <h2>hi react</h2>
-    <Jokes />
-  </div>
-
+  return (
+    <div className='App'>
+      <h2>hi react</h2>
+      <Jokes />
+    </div>
+  )
 }
 
 export default App;
